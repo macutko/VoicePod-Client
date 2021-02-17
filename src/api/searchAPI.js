a6e3715762c73eeb3aee607dd781b6c0874e6bc0
Matus Gallik <matusgallik008@gmail.com>
2021-02-17 09:38:43 +0100

    fix: searchAPI.js
    Former-commit-id: a4df6d3a3973ebbda071fc0e46e6ca13097b956a

--- src/api/searchAPI.js
+++ src/api/searchAPI.js
@@ -1,7 +1,7 @@
 export const searchAPI = ({socket, data}) => {
     return new Promise((fulfill, reject) => {
 
-        socket.emit('search', data, (error, response) => {
+        socket.emit('searchUser', data, (error, response) => {
             if (error) {
                 console.log(`Error in search ${error}`)
                 reject({})
