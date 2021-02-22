741be6af80e3d2ff76cc34e60d1f33caf72f5667
Matus <matusgallik008@gmail.com>
2021-02-22 16:25:20 +0000

    Resolve "BusinessProfile ActivationAPI"

---
+++ src/components/molecules/SettingsTab/ToggleBusinessProfile.js
@@ -0,0 +1,38 @@
+import {List, Switch} from "react-native-paper";
+import GlobalContext from "../../atoms/GlobalState";
+import {useContext, useEffect, useRef} from "react/cjs/react.production.min";
+import React from "react";
+
+const ToggleBusinessProfile = ({submitUpdate}) => {
+    const context = useContext(GlobalContext)
+    const _isMounted = useRef(true)
+    console.log(Object.keys(context))
+
+    useEffect(() => {
+
+        return () => {
+            _isMounted.current = false
+        }
+    }, []);
+
+    const toggleBusinessAccount = () => {
+        //TODO: make this isMounted and isFetching compliable
+        //TODO: first add a payment method
+        submitUpdate({
+            businessActivated: !context.user.businessActivated
+        }).then((r) => {
+                // refreshState(this.props.globalState.token)
+            }
+        ).catch(e => console.log(`Error in Payments ${e}`))
+
+    }
+
+
+    return (
+        <List.Item title="Business account"
+                   right={() => <Switch value={context.user.businessActivated}
+                                        onValueChange={toggleBusinessAccount}/>}/>
+    )
+}
+
+export default ToggleBusinessProfile
