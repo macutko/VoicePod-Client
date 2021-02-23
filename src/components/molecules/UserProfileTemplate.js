e9e2741e16479402f179edd69fc33155b625e4f2
Juraj Bublinec <jurajbublinec@gmail.com>
2021-02-23 16:16:52 +0100

    feat: style user profile

--- src/components/molecules/UserProfileTemplate.js
+++ src/components/molecules/UserProfileTemplate.js
@@ -1,52 +1,57 @@
-import React from "react"
-import { StyleSheet, View } from "react-native"
-import { colorScheme } from "../../constants/Colors"
-import AvatarCustom from "../atoms/AvatarCustom"
-import Metrics from "../atoms/Metrics"
-import StarRating from "../atoms/StarRating"
-import TitleCustom from "../atoms/TitleCustom"
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { colorScheme } from "../../constants/Colors";
+import AvatarCustom from "../atoms/AvatarCustom";
+import Metrics from "../atoms/Metrics";
+import StarRating from "../atoms/StarRating";
+import TitleCustom from "../atoms/TitleCustom";
 
 const UserProfileTemplate = (props) => {
-	return (
-		<View style={styles.containerStyle}>
-			<View style={styles.backgroundColor} />
-			<View style={styles.avatar}>
-				<AvatarCustom size={180} />
-			</View>
-			<View style={styles.content}>
-				<TitleCustom center space>
-					{props.firstName} {props.lastName}
-				</TitleCustom>
-				<StarRating rating={props.rating} />
-				<Metrics
-					reviews={props.reviews}
-					cases={props.cases}
-					budget={props.budget}
-				/>
-				{props.children}
-			</View>
-		</View>
-	)
-}
-export default UserProfileTemplate
+    return (
+        <View style={styles.containerStyle}>
+            <View style={styles.avatarContainer}>
+                <View style={styles.avatarColorStripe}>
+                    <View style={styles.avatar}>
+                        <AvatarCustom size={180} />
+                    </View>
+                </View>
+            </View>
+
+            <TitleCustom center>
+                {props.firstName} {props.lastName}
+            </TitleCustom>
+            <StarRating rating={props.rating} />
+            <Metrics
+                reviews={props.reviews}
+                cases={props.cases}
+                budget={props.budget}
+            />
+            {props.children}
+        </View>
+    );
+};
+export default UserProfileTemplate;
 
 const styles = StyleSheet.create({
-	containerStyle: {
-		alignItems: "center",
-		backgroundColor: colorScheme.background,
-	},
-	content: {
-		paddingHorizontal: 20,
-		width: "100%",
-		alignItems: "center",
-	},
-	backgroundColor: {
-		backgroundColor: colorScheme.primary,
-		height: 160,
-		width: "100%",
-	},
-
-	avatar: {
-		marginTop: -120,
-	},
-})
+    containerStyle: {
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: colorScheme.background,
+        paddingHorizontal: "5%",
+    },
+    avatarContainer: {
+        height: 220,
+        width: "112%",
+        flexDirection: "row",
+    },
+    avatarColorStripe: {
+        backgroundColor: colorScheme.primary,
+        height: 160,
+        width: "100%",
+        alignItems: "center",
+    },
+    avatar: {
+        marginTop: 30,
+    },
+});
